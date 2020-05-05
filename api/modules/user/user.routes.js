1599ebaaf4ba41cec456280ce1097048e21f7ff2
Pritam Kamble <7276279026.pk@gmail.com>
2020-05-05 23:50:04 +0530

    finduser id removed

--- api/modules/user/user.routes.js
+++ api/modules/user/user.routes.js
@@ -10,8 +10,6 @@ router.post("/register", userRegisterValidation, throwValidationError, UserContr
 
 router.post("/login", UserController.loginUser);
 
-router.post("/findId", UserController.findUserId);
-
 router.post("/forgetpass", UserController.forgotpassUser);
 
 router.post("/userConfirmpass", UserController.confirmpassUser);

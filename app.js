a65d466b960509853a8ee9b78fd0f7282f7e2ab4
Vini Mehta <vini.mehta78@gmail.com>
2020-05-17 18:03:33 +0530

    corrections done

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const instituteRoutes = require('./api/modules/institute/institute.routes');
 const inquiryRoutes = require('./api/modules/inquiry/inquiry.routes');
 
 
-mongoose.connect("mongodb+srv://vinidb:vinidbpass@cluster0-nrlvv.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://root:root@cluster0-4hhue.mongodb.net/test1?retryWrites=true&w=majority", { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 app.use(morgan("dev"));

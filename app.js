b87b0b35cdfde14e73d1b6b8d28db1fe55855352
Shubham-Gawade <shubham.gawade7292@gmail.com>
2020-05-20 03:56:45 +0530

    Database Link Change 1

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const courseRoutes = require('./api/modules/course/course.routes');
 const instituteRoutes = require('./api/modules/institute/institute.routes');
 const inquiryRoutes = require('./api/modules/inquiry/inquiry.routes');
 
-
-mongoose.connect("mongodb+srv://shubhamg_7292:gawade2927@cluster0-cr1db.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://root:root@cluster0-4hhue.mongodb.net/test1?retryWrites=true&w=majority", { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 app.use(morgan("dev"));
